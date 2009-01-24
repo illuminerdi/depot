27018b75d0d603c2a06ae58124edca0a71cd8760
Joshua Clingenpeel <joshua.clingenpeel@gmail.com>
2009-01-23 17:30:38 -0800

    adding store controller tests provided by Aaron for week 2 assignment

---
+++ test/functional/store_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class StoreControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:products)
+
+    Product.find_products_for_sale.each do |product|
+      assert_tag :tag => 'h3', :content => product.title
+      assert_match /#{sprintf("%01.2f", product.price / 100)}/, @response.body
+    end
+  end
+end
