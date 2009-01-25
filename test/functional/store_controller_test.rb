934d973f75ce92aba1d08278b8d5753aa2850368
Joshua Clingenpeel <joshua.clingenpeel@gmail.com>
2009-01-25 00:23:51 -0800

    tweaking the layout and l&f, modified store controller tests, added new one to check for existence of 'add to cart' button.

--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -9,6 +9,14 @@ class StoreControllerTest < ActionController::TestCase
     Product.find_products_for_sale.each do |product|
       assert_tag :tag => 'h3', :content => product.title
       assert_match /#{sprintf("%01.2f", product.price / 100)}/, @response.body
+      assert_tag :tag => 'input', :attributes => {
+        :value => "Add to Cart",
+        :type => "submit"
+      }
+      assert_tag :tag => 'form', :attributes => {
+        :method => 'post'
+      }
+      assert_tag :tag => 'a'
     end
   end
 end
