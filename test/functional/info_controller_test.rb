52484869ff8c4d264ae1ae7e8d61c16e6d7d3973
Joshua Clingenpeel <joshua.clingenpeel@gmail.com>
2009-02-16 08:10:45 -0800

    adding initial tests for the info controller.

--- test/functional/info_controller_test.rb
+++ test/functional/info_controller_test.rb
@@ -1,8 +1,17 @@
 require 'test_helper'
 
 class InfoControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  test "who_bought works for xml info" do
+    get :who_bought, :id => products(:one).id
+    product = assigns(:product)
+    orders = assigns(:orders)
+    
+    assert_response :success
+    assert_match /<title>Foo is the first book<\/title>/, @response.body
+    assert_match /<name>Joshua Clingenpeel<\/name>/, @response.body
+    assert_match /<address>123 Happy Road, Seattle WA 98101/, @response.body
+    assert_match /<description>A quick book for testing stuff!/, @response.body
   end
+
 end
