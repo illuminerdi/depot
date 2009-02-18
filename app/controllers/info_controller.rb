d417751c6865f134468116defe38be235db1e31c
Joshua Clingenpeel <joshua.clingenpeel@gmail.com>
2009-02-17 17:33:27 -0800

    more coding/testing fun with the info controller.

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -1,10 +1,10 @@
 class InfoController < ApplicationController
   def who_bought
     @product = Product.find(params[:id])
-    @orders = @product.orders
     
     respond_to do |format|
-      format.xml { render :layout => false }
+      format.xml { render :layout => false, :xml => @product.to_xml(:include => :orders) }
+      format.json { render :layout => false, :json => @product.to_json(:include => :orders )}
     end
   end
 
--- test/functional/info_controller_test.rb
+++ test/functional/info_controller_test.rb
@@ -8,10 +8,14 @@ class InfoControllerTest < ActionController::TestCase
     orders = assigns(:orders)
     
     assert_response :success
-    assert_match /<title>Foo is the first book<\/title>/, @response.body
+    assert_match /<title>#{product.title}<\/title>/, @response.body
     assert_match /<name>Joshua Clingenpeel<\/name>/, @response.body
     assert_match /<address>123 Happy Road, Seattle WA 98101/, @response.body
     assert_match /<description>A quick book for testing stuff!/, @response.body
+  end
+  
+  test "who_bought works for json info" do
+    get :who_bought, :id => products(:one).id, :format => "json"
   end
 
 end
